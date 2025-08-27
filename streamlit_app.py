90ba5a01f129d9ded9b0c0467a78ac399a802ece
Jeeten7 <36022446+Jeeten7@users.noreply.github.com>
2025-08-27 10:19:35 +0400

    Update streamlit_app.py
    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,11 @@ if ingredients_list:
         values('""" + ingredients_string + """' ,'""" +name_on_order +"""')"""
     #st.write(my_insert_statement)
     #st.stop
-    
+
+    import requests
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    st.text(smoothiefroot_response)
+
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
     #if ingredients_string:
